8affe84dbf749ed8291db2efa74d5fb999bdac6d
lutzer <lutz@Lutzs-MacBook-Pro.local>
2016-02-05 18:10:31 +0100

    added reverse switch to arduino sketch

--- scripts/shredder/shredder.ino
+++ scripts/shredder/shredder.ino
@@ -1,31 +1,57 @@
-#define SWITCH_PIN 12
+#define SWITCH_PIN 11
+#define REWIND_SWITCH_PIN 12
 #define LED_PIN 13
 #define SERVO_PIN 9
 
 #include <Servo.h>
 
+enum ShredderDirection {
+  FORWARD,
+  BACKWARD,
+  STOP
+};
+
 Servo servo;
 
 void setup() {
   pinMode(SWITCH_PIN, INPUT_PULLUP);
+  pinMode(REWIND_SWITCH_PIN, INPUT_PULLUP);
   pinMode(LED_PIN, OUTPUT);
-
-  servo.attach(SERVO_PIN);
-
 }
 
 void loop() {
 
-  int sensorVal = digitalRead(SWITCH_PIN);
+  int switchVal = digitalRead(SWITCH_PIN);
+  int reverseSwitchVal = digitalRead(REWIND_SWITCH_PIN);
   
-  if (sensorVal == HIGH) {
+  if (switchVal == HIGH && reverseSwitchVal == HIGH) {
     digitalWrite(LED_PIN, LOW);
-    //servo.attach(SERVO_PIN);
-    servo.write(180);
+    setShredderDirection(FORWARD);
+  } else if ( reverseSwitchVal == LOW) {
+    setShredderDirection(BACKWARD);
   } else {
     digitalWrite(LED_PIN, HIGH);
-    servo.write(100);
+    setShredderDirection(STOP);
   }
 
   delay(15);
 }
+
+void setShredderDirection(ShredderDirection direction) {
+  
+  if (direction == FORWARD) { // forward servo
+    if (!servo.attached())
+      servo.attach(SERVO_PIN);
+    servo.write(0);
+  } else if ( direction == BACKWARD ) { // rewind servo
+    if (!servo.attached())
+      servo.attach(SERVO_PIN);
+    servo.write(180);
+  } else { // stop servo
+    if (servo.attached()) {
+      servo.write(90);
+      servo.detach();
+    }
+  } 
+}
+
